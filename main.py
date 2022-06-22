590460a3006e2e0ff7d63850119d708b09c93bb8
Sara Lins <saranicoly0@gmail.com>
2022-06-22 12:46:15 -0300

    refactoring main

--- main.py
+++ main.py
@@ -2,18 +2,19 @@ import requests, os
 from datetime import datetime
 
 
-def api_get(my_headers):
-    """Get activities from the API"""
-    try:
-        return requests.get(
-            "https://api.slangapp.com/challenges/v1/activities", headers=my_headers
-        ).json()
-    except Exception as e:
-        print("An error ocurred: ", e)
-        return None
+def iso_string_to_datetime(time):
+    """Transform from ISO to datetime"""
+    return datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
 
 
-def activities_by_user(data):
+def diff_to_sec(time1, time2):
+    """Transform the difference between two times to seconds"""
+    time = iso_string_to_datetime(time1) - iso_string_to_datetime(time2)
+
+    return time.total_seconds()
+
+
+def group_activities_by_user(data):
     """Group all activities by user and order by first_seen_at"""
     user_activities = {}
     for activity in data["activities"]:  # O(n)
@@ -30,30 +31,18 @@ def activities_by_user(data):
     return user_activities
 
 
-def transform_time(time):
-    """Transform str to time"""
-    return datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
-
-
-def diff_to_sec(time1, time2):
-    """Transform the difference between two times to seconds"""
-    time = transform_time(time1) - transform_time(time2)
-
-    return time.total_seconds()
-
-
 def create_user_sessions(activities_by_user):
     """Create a dict with user_id as key and a list of sessions as value"""
-    user_sessions = {"user_sessions": {}}
+    user_sessions = {}
     for user_id, activities in activities_by_user.items():  # O(n) x O(m) = O(n x m)
-        user_sessions["user_sessions"][user_id] = []
+        user_sessions[user_id] = []
         for activity in activities:
             if (
-                not user_sessions["user_sessions"][user_id]
+                not user_sessions[user_id]
                 or (
                     diff_to_sec(
                         activity["first_seen_at"],
-                        user_sessions["user_sessions"][user_id][-1]["ended_at"],
+                        user_sessions[user_id][-1]["ended_at"],
                     )
                 )
                 > 300
@@ -66,21 +55,28 @@ def create_user_sessions(activities_by_user):
                         activity["answered_at"], activity["first_seen_at"]
                     ),
                 }
-                user_sessions["user_sessions"][user_id].append(temp_dict)
+                user_sessions[user_id].append(temp_dict)
             else:
-                user_sessions["user_sessions"][user_id][-1]["ended_at"] = activity[
-                    "answered_at"
-                ]
-                user_sessions["user_sessions"][user_id][-1]["activity_ids"].append(
-                    activity["id"]
+                user_sessions[user_id][-1]["ended_at"] = activity["answered_at"]
+                user_sessions[user_id][-1]["activity_ids"].append(activity["id"])
+                user_sessions[user_id][-1]["duration_seconds"] = diff_to_sec(
+                    user_sessions[user_id][-1]["ended_at"],
+                    user_sessions[user_id][-1]["started_at"],
                 )
-                user_sessions["user_sessions"][user_id][-1][
-                    "duration_seconds"
-                ] = diff_to_sec(
-                    user_sessions["user_sessions"][user_id][-1]["ended_at"],
-                    user_sessions["user_sessions"][user_id][-1]["started_at"],
-                )
-    return user_sessions
+    return {"user_sessions": user_sessions}
+
+
+def api_get(my_headers):
+    """Get activities from the API"""
+    req = requests.get(
+        "https://api.slangapp.com/challenges/v1/activities", headers=my_headers
+    )
+    if req.status_code == 200:
+        print("Successfully got activities from the API")
+        return req.json()
+    else:
+        print("An error ocurred. Status Code: ", req.status_code)
+        req.raise_for_status()
 
 
 def api_post(user_sessions, my_headers):
@@ -95,13 +91,14 @@ def api_post(user_sessions, my_headers):
         print("Successfully posted user_sessions to the API")
     else:
         print("An error ocurred. Status Code: ", req.status_code)
+        req.raise_for_status()
 
 
 if __name__ == "__main__":
     my_headers = {"Authorization": f'Bearer {os.environ["API_TOKEN"]}'}
 
     initial_data = api_get(my_headers)
-    user_activities = activities_by_user(initial_data)
+    user_activities = group_activities_by_user(initial_data)
     user_sessions = create_user_sessions(user_activities)
     api_post(user_sessions, my_headers)
     print("\nEnd of program")
