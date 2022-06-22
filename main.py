963194cda1dda060ce792afd14486574b533947f
Sara Lins <saranicoly0@gmail.com>
2022-06-21 22:21:14 -0300

    sort activities by first seen at

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ for activity in response['activities']:
   if activity['user_id'] not in user_activities:
     user_activities[activity['user_id']] = []
   user_activities[activity['user_id']].append(activity)
+  
+# order user activities by their first_seen_at
+for user_id in user_activities:
+  user_activities[user_id] = sorted(user_activities[user_id], key=lambda k: k['first_seen_at'])
+
+print(user_activities)
