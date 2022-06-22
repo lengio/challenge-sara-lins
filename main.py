54cb239ed33fecc8abee417dc6158b624ce1164b
Sara Lins <saranicoly0@gmail.com>
2022-06-22 12:09:57 -0300

    divide code into functions

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import requests, os
 from datetime import datetime
 
-my_headers = {"Authorization": f'Bearer {os.environ["API_TOKEN"]}'}
 
-
-def api_get():
+def api_get(my_headers):
     """Get activities from the API"""
     try:
         return requests.get(
@@ -15,10 +13,10 @@ def api_get():
         return None
 
 
-def activities_by_user():
+def activities_by_user(data):
     """Group all activities by user and order by first_seen_at"""
     user_activities = {}
-    for activity in api_get()["activities"]:  # O(n)
+    for activity in data["activities"]:  # O(n)
         if activity["user_id"] not in user_activities:
             user_activities[activity["user_id"]] = []
         user_activities[activity["user_id"]].append(activity)
@@ -33,53 +31,77 @@ def activities_by_user():
 
 
 def transform_time(time):
-    """Transform time to a readable format"""
+    """Transform str to time"""
     return datetime.strptime(time, "%Y-%m-%dT%H:%M:%S.%f%z")
 
 
-user_sessions = {"user_sessions": {}}
-for user_id, activities in activities_by_user().items():  # O(n) x O(m) = O(n x m)
-    user_sessions["user_sessions"][user_id] = []
-    for activity in activities:
-        if (
-            not user_sessions["user_sessions"][user_id]
-            or (
-                transform_time(activity["first_seen_at"])
-                - transform_time(
-                    user_sessions["user_sessions"][user_id][-1]["ended_at"]
-                )
-            ).total_seconds()
-            > 300
-        ):
-            temp_dict = {
-                "started_at": activity["first_seen_at"],
-                "ended_at": activity["answered_at"],
-                "activity_ids": [activity["id"]],
-                "duration_seconds": (
-                    transform_time(activity["answered_at"])
-                    - transform_time(activity["first_seen_at"])
-                ).total_seconds(),
-            }
-            user_sessions["user_sessions"][user_id].append(temp_dict)
-        else:
-            user_sessions["user_sessions"][user_id][-1]["ended_at"] = activity[
-                "answered_at"
-            ]
-            user_sessions["user_sessions"][user_id][-1]["activity_ids"].append(
-                activity["id"]
-            )
-            user_sessions["user_sessions"][user_id][-1]["duration_seconds"] = (
-                transform_time(user_sessions["user_sessions"][user_id][-1]["ended_at"])
-                - transform_time(
-                    user_sessions["user_sessions"][user_id][-1]["started_at"]
-                )
-            ).total_seconds()
+def diff_to_sec(time1, time2):
+    """Transform the difference between two times to seconds"""
+    time = transform_time(time1) - transform_time(time2)
 
-# complexity: O(n) + O(n x m_log_m) + O(n x m) = O(n x m_log_m), where n is the number of users and m is the number of activities for each user
+    return time.total_seconds()
 
-requests.post(
-    "https://api.slangapp.com/challenges/v1/activities/sessions",
-    headers=my_headers,
-    json=user_sessions,
-    timeout=5,
-)
+
+def create_user_sessions(activities_by_user):
+    """Create a dict with user_id as key and a list of sessions as value"""
+    user_sessions = {"user_sessions": {}}
+    for user_id, activities in activities_by_user.items():  # O(n) x O(m) = O(n x m)
+        user_sessions["user_sessions"][user_id] = []
+        for activity in activities:
+            if (
+                not user_sessions["user_sessions"][user_id]
+                or (
+                    diff_to_sec(
+                        activity["first_seen_at"],
+                        user_sessions["user_sessions"][user_id][-1]["ended_at"],
+                    )
+                )
+                > 300
+            ):
+                temp_dict = {
+                    "started_at": activity["first_seen_at"],
+                    "ended_at": activity["answered_at"],
+                    "activity_ids": [activity["id"]],
+                    "duration_seconds": diff_to_sec(
+                        activity["answered_at"], activity["first_seen_at"]
+                    ),
+                }
+                user_sessions["user_sessions"][user_id].append(temp_dict)
+            else:
+                user_sessions["user_sessions"][user_id][-1]["ended_at"] = activity[
+                    "answered_at"
+                ]
+                user_sessions["user_sessions"][user_id][-1]["activity_ids"].append(
+                    activity["id"]
+                )
+                user_sessions["user_sessions"][user_id][-1][
+                    "duration_seconds"
+                ] = diff_to_sec(
+                    user_sessions["user_sessions"][user_id][-1]["ended_at"],
+                    user_sessions["user_sessions"][user_id][-1]["started_at"],
+                )
+    return user_sessions
+
+
+def api_post(user_sessions, my_headers):
+    """Post user_sessions to the API"""
+    req = requests.post(
+        "https://api.slangapp.com/challenges/v1/activities/sessions",
+        headers=my_headers,
+        json=user_sessions,
+        timeout=5,
+    )
+    if req.status_code == 204:
+        print("Successfully posted user_sessions to the API")
+    else:
+        print("An error ocurred. Status Code: ", req.status_code)
+
+
+if __name__ == "__main__":
+    my_headers = {"Authorization": f'Bearer {os.environ["API_TOKEN"]}'}
+
+    initial_data = api_get(my_headers)
+    user_activities = activities_by_user(initial_data)
+    user_sessions = create_user_sessions(user_activities)
+    api_post(user_sessions, my_headers)
+    print("\nEnd of program")
