88d7ebeea722df1f06573a88cb5c57b4d83088c5
Sara Lins <saranicoly0@gmail.com>
2022-06-21 22:00:27 -0300

    order for users

--- main.py
+++ main.py
@@ -4,4 +4,8 @@ import os
 my_headers = {'Authorization' : f'Bearer {os.environ["API_TOKEN"]}'}
 response = requests.get('https://api.slangapp.com/challenges/v1/activities', headers=my_headers).json()
 
-print(response)
+user_sessions = {}
+for activity in response['activities']:
+  if activity['user_id'] not in user_sessions:
+    user_sessions[activity['user_id']] = []
+  user_sessions[activity['user_id']].append(activity)
