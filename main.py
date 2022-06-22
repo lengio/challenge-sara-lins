b77680935dbb86916dea0023c2f171b193b0859d
Sara Lins <saranicoly0@gmail.com>
2022-06-21 22:02:53 -0300

    rename user_session

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import os
 my_headers = {'Authorization' : f'Bearer {os.environ["API_TOKEN"]}'}
 response = requests.get('https://api.slangapp.com/challenges/v1/activities', headers=my_headers).json()
 
-user_sessions = {}
+user_activities = {}
 for activity in response['activities']:
-  if activity['user_id'] not in user_sessions:
-    user_sessions[activity['user_id']] = []
-  user_sessions[activity['user_id']].append(activity)
+  if activity['user_id'] not in user_activities:
+    user_activities[activity['user_id']] = []
+  user_activities[activity['user_id']].append(activity)
